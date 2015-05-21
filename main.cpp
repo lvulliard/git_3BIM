01edc8214006d007fc541ead3377b1bb24aec864
Loan Vulliard <loan.vulliard@insa-lyon.fr>
2015-05-21 11:55:48 +0200

    Added a title and an icon to the window

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
-#include <gtkmm.h>
+#include <gtkmm/main.h>
+#include <gtkmm/window.h>
 
 int main(int argc, char* argv[]) {
+	// Initiate Gtk
     Gtk::Main app(argc, argv);
+    // Create the Window object
     Gtk::Window fenetre;
+    // Set the title
+    fenetre.set_title("Coevolution model");
+    // Set icon
+    fenetre.set_icon_from_file("icon.png");
+    // Display the Window object and start the main loop
     Gtk::Main::run(fenetre);
     return 0;
 }
