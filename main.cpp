802ec4cf78040875d6a3d54b4c70e8a885473f6b
vnieto <vincent.nieto@wanadoo.fr>
2015-05-28 11:47:08 +0200

    Creation of Main_Window files

--- main.cpp
+++ main.cpp
@@ -22,6 +22,8 @@
 #include "Environment.h"
 #include "Host.h"
 #include "DefVal.h"
+#include "Main_Window.h"
+
 // ===========================================================================
 //                         Declare Miscellaneous Functions
 // ===========================================================================
@@ -41,20 +43,7 @@ int main(int argc, char* argv[]) {
     // Initiate Gtk
     Gtk::Main app(argc, argv);
     // Create the Window object
-    Gtk::Window fenetre;
-    // Set the title
-    fenetre.set_title(DefVal::APP_NAME);
-    // Set icon
-    fenetre.set_icon_from_file(DefVal::ICON);
-    // Set window size
-    fenetre.resize(DefVal::WINDOW_WIDTH, DefVal::WINDOW_HEIGHT);
-    // Window position
-    fenetre.set_position(Gtk::WIN_POS_CENTER);
-    fenetre.set_border_width(DefVal::WINDOW_BORDER_WIDTH);
-    // Add button
-    Gtk::Button bouton(DefVal::BUTTON1_LABEL); 
-    fenetre.add(bouton); 
-    bouton.show(); 
+    Main_Window fenetre;
 
     // Display the Window object and start the main loop
     Gtk::Main::run(fenetre);
--- makefile
+++ makefile
@@ -1,14 +1,13 @@
 all: coevolution.out
 
 coevolution.out: main.cpp DefVal.h Environment.o Host.o Main_Window.o
-	g++ -o coevolution.out -Wall Environment.o Host.o main.cpp `pkg-config gtkmm-3.0 --cflags --libs`
-
+	g++ -o coevolution.out -Wall Main_Window.o Environment.o Host.o main.cpp `pkg-config gtkmm-3.0 --cflags --libs`
 
 Environment.o: Environment.cpp Environment.h
 	g++ -o Environment.o -c -Wall Environment.cpp
 
 Main_Window.o: Main_Window.cpp Main_Window.h DefVal.h
-	g++ -o Main_Window.o -c -Wall Main_Window.cpp
+	g++ -o Main_Window.o -c -Wall Main_Window.cpp `pkg-config gtkmm-3.0 --cflags --libs`
 
 Host.o: Host.cpp Host.h
 	g++ -o Host.o -c -Wall Host.cpp
