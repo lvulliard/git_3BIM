40506f9fa34f443846036ba0c82eaf3d6cde5627
mlebre <marianne.lebre@insa-lyon.fr>
2015-06-11 11:05:32 +0200

    Nothing changes

--- Environment.cpp
+++ Environment.cpp
@@ -19,7 +19,7 @@
 // ===========================================================================
 
 #include "Environment.h"
-
+#include "DefVal.h"
 
 //############################################################################
 //                                                                           #
@@ -34,7 +34,8 @@
 
 Environment::Environment(void)
 {
-  profile = 0;
+  profile = profileFunction();
+
   hosts = NULL;
 }
 
@@ -70,11 +71,24 @@ void Environment::saveLog(void)
 {
 }
 
+unsigned int** Environment::profileFunction(void) 
+{
+  unsigned int** profile;
+  profile = new unsigned int*[DefVal::PIC_WIDTH]; // Width of the simulation
+  for (int i=0; i<DefVal::PIC_WIDTH; i++) {
+    profile[i] = new unsigned int[DefVal::PIC_HEIGHT];  // Each pointer points to an array of the height of the picture
+  }
+  
+  for (int x=0; x<DefVal::PIC_WIDTH; x++) { // parse on x
+    for (int y=0; y<DefVal::PIC_HEIGHT; y++) { // parse on y
+   	 if (y <= int((DefVal::PIC_HEIGHT*130/500)*(0.5*cos( ( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85) +cos(3*(( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85))+0.5*sin(7*(( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85)) +0.5*sin((( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85)+1.71))) ) {
+   	 	profile[x][y] = 1;
+   	 }
+   	 else {
+   	 	profile[x][y] = 0;
+   	 }
+    }
+  }
 
-
-
-
-
-
-
-
+  return profile;
+}
