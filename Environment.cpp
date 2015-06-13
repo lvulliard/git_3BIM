90af1cd796e35fe466c2e1b6dec479c03ad6f36e
Loan Vulliard <loan.vulliard@insa-lyon.fr>
2015-06-13 15:54:38 +0200

    Corrected environment function calculation

--- Environment.cpp
+++ Environment.cpp
@@ -83,7 +83,7 @@ void Environment::saveLog(void)
 }
 
 unsigned int** Environment::profileFunction(void) 
-{
+{ 
   unsigned int** profile;
   profile = new unsigned int*[DefVal::PIC_WIDTH]; // Width of the simulation
   for (int i=0; i<DefVal::PIC_WIDTH; i++) {
@@ -92,7 +92,7 @@ unsigned int** Environment::profileFunction(void)
   
   for (int x=0; x<DefVal::PIC_WIDTH; x++) { // parse on x
     for (int y=0; y<DefVal::PIC_HEIGHT; y++) { // parse on y
-   	 if (y <= int((DefVal::PIC_HEIGHT*130/500)*(0.5*cos( ( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85) +cos(3*(( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85))+0.5*sin(7*(( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85)) +0.5*sin((( x /( (DefVal::PIC_WIDTH/500)*82.62) ) - 2.85)+1.71))) ) {
+   	 if (y <= int(( double(DefVal::PIC_HEIGHT)*130.1/500.1)*(0.5*cos( ( x /( (DefVal::PIC_WIDTH/500.1)*90.1) ) - 2.85) +cos(3*(( x /( (DefVal::PIC_WIDTH/500.1)*90.1) ) - 2.85))+0.5*sin(7*(( x /( (DefVal::PIC_WIDTH/500.1)*90.1) ) - 2.85)) +0.5*sin((( x /( (DefVal::PIC_WIDTH/500.1)*90.1) ) - 2.85)+1.71)) + 130 ) ) {
    	 	profile[x][y] = 1;
    	 }
    	 else {
